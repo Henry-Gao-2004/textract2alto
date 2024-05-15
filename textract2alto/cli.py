f69353ccac1f42d6a1d69e2179df71ac846c202a
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2024-05-15 17:33:14 -0400

    Allow processing all files in the folder

--- textract2alto/cli.py
+++ textract2alto/cli.py
@@ -2,6 +2,7 @@ import click
 import subprocess
 import os
 import sys
+import glob
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -25,15 +26,19 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('--region-order', default='document', help="Order in which to iterate over the regions", type=click.Choice(['document', 'reading-order', 'reading-order-only']))
 @click.option('--textline-order', default='document', help="Order in which to iterate over the textlines", type=click.Choice(['document', 'index', 'textline-order']))
 @click.option('--timestamp-src', default='LastChange', help="Which element to use for the timestamp", type=click.Choice(['Created', 'LastChange', 'none']))
-@click.argument("path", type=click.Path(dir_okay=False, exists=True))
+@click.argument("path", type=click.Path(dir_okay=True, exists=True))
 def cli(path, alto_version, check_words, check_border, skip_empty_lines, trailing_dash_to_hyp, dummy_textline, dummy_word, 
          textequiv_index, textequiv_fallback_strategy, region_order, textline_order, timestamp_src):
-    """Convert an AWS Textract JSON file to a PAGE XML file.
+     
+     """Convert an AWS Textract JSON file to a PAGE XML file.
 
-    Also requires the original input image of AWS OCR to get absolute image coordinates."""
+     Also requires the original input image of AWS OCR to get absolute image coordinates."""
     
-    convert_complex(path, alto_version, check_words, check_border, skip_empty_lines, trailing_dash_to_hyp, dummy_textline, dummy_word, 
-         textequiv_index, textequiv_fallback_strategy, region_order, textline_order, timestamp_src)
+     if path.endswith("jpg"):
+          convert_complex(path, alto_version, check_words, check_border, skip_empty_lines, trailing_dash_to_hyp, dummy_textline, dummy_word, textequiv_index, textequiv_fallback_strategy, region_order, textline_order, timestamp_src)
+     elif os.path.isdir(path):
+          for filename in glob.glob(path+'/*.jpg'):
+               convert_complex(filename, alto_version, check_words, check_border, skip_empty_lines, trailing_dash_to_hyp, dummy_textline, dummy_word, textequiv_index, textequiv_fallback_strategy, region_order, textline_order, timestamp_src)
 
 if __name__ == "__main__":
     cli()
